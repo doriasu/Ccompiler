9154251a8a7d406d58e33b07097522a6d3976e1d
doriasu <takuandtetsu@live.jp>
2019-10-01 09:14:31 +0900

    talknizerの作成とMakefileの修正

--- 9cc.c
+++ 9cc.c
@@ -1,31 +1,120 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdbool.h>
+#include<string.h>
 #include<stdlib.h>
-int main(int argc,char**argv){
-    if(argc!=2){
-      fprintf(stderr, "引数の個数が正しくありません。\n");
-      return 1;
+//とーくんの種類だよ〜
+typedef enum {
+  TK_RESERVED,  //記号
+  TK_NUM,       //整数トークン
+  TK_EOF,       //入力の終わりを表すトークン
+} TokenKind;
+typedef struct Token Token;
+//うえの定義の中身
+struct Token {
+  TokenKind kind;//トークンの型
+  Token *next;
+  int val;//kindがTK_NUMの時、その数値
+  char *str;//トークン文字列
+};
+//現在着目nowのトークン
+Token *token;
+
+//エラー報告関数
+//printfと同じ引数
+void error(char *fmt, ...) { 
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+//次のトークンが期待している記号のときには、トークンを1強み勧めて
+//真を返す。それ以外は義を返す
+bool consume(char op){
+  if(token->kind!=TK_RESERVED||token->str[0]!=op){
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+//次のトークンが期待している記号のときには、トークンを1強み勧めて
+//真を返す。それ以外はエラーを報告
+void expect(char op){
+  if(token->kind!=TK_RESERVED||token->str[0]!=op){
+    error("'%c'じゃないよー", op);
+  }
+  token = token->next;
+  
+}
+//次のトークンが数値の時、トークンを一つ読み進めて、その数値を返す
+//それ以外はエラーを返す
+int expect_number(){
+  if(token->kind!=TK_NUM){
+    error("数じゃないけど???");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+bool at_eof() { return token->kind == TK_EOF; }
+//新しいトークンを作成してcurにつなげる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  cur->next = tok;
+  tok->kind = kind;
+  tok -> str = str;
+  return tok;
+}
+Token *tokenize(char *p) { 
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+  while(*p){
+    //空白の検出
+    if(isspace(*p)){
+      p++;
+      continue;
     }
-    char *p = argv[1];
-    printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
-    printf("    mov rax,%ld\n", strtol(p,&p,10));
-    while(*p){
-      if(*p=='+'){
-        p++;
-        printf("  add rax,%ld\n", strtol(p, &p, 10));
-      }else if(*p=='-'){
-        p++;
-        printf("  sub rax,%ld\n", strtol(p, &p, 10));
-      }else{
-        fprintf(stderr, "予期しない文字です:'%c'\n", *p);
-        return 1;
-      }
+    if(isdigit(*p)){
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
     }
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+    error("トークナイズできんが...");
+  }
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error("引数の個数が正しくありません。");
+    return 1;
+  }
+  token = tokenize(argv[1]);
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  //式の始まりは数値でないと困る
+  printf("    mov rax,%d\n", expect_number());
+  while(!at_eof()){
+    //式の終わりでにない時ループ続行
+    if(consume('+')){
+      printf("  add rax,%d\n", expect_number());
+      continue;
+    }
+    if(consume('-')) { 
+      printf("  sub rax,%d\n", expect_number());
+      continue;
+    }
+  }
 
-
-
-    printf("    ret\n");
-    return 0;
+  printf("    ret\n");
+  return 0;
 }
--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CFLAGS=-std=c11 -g static
 9cc:9cc.c
+	gcc -o 9cc 9cc.c
 test:9cc
 	./test.sh
 clean:
