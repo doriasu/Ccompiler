793a813d27c4424f350cabf4e2013634d7ae2bcb
doriasu <takuandtetsu@live.jp>
2019-10-11 15:47:47 +0900

    写経終わりデバッグ開始

--- main.c
+++ main.c
@@ -5,19 +5,29 @@ int main(int argc, char **argv) {
     error("引数の個数が正しくありません。");
     return 1;
   }
-  token = tokenize(argv[1]);
   user_input = argv[1];
-  Node *node=expr();
+  tokenize();
+  program();
   //アセンブリの前半部分の出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
   
+  //プロローグ
+  //変数26こぶんの領域確保
+  printf("	push rbp\n");
+  printf("	mov rbp,rsp\n");
+  printf("	sub rsp,208\n");
   //抽象構文木をいじいじ
-  gen(node);
+  for(int i=0;code[i];i++){
+	  gen(code[i]);
+	  //式の評価結果としてスタックに一つ値が残っているので、スタックが溢れないようにpop
+	  printf("	pop rax\n");
+  }
 
   //スタックトップに式全体の値が有るよ〜
-  printf("  pop rax\n");
+  printf("	mov rsp,rbp\n");
+  printf("	pop rbp\n");
   printf("  ret\n");
   return 0;
 }
